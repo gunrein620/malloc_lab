b5d21cd23e4c3e340905ce3a6984ae177faa237a
gunrein <gunrein620@gmail.com>
2026-04-14 11:31:47 +0900

    docs: add 한글 주석

--- malloc-lab/README.md
+++ malloc-lab/README.md
@@ -50,3 +50,56 @@ To get a list of the driver flags:
 
 	unix> mdriver -h
 
+#####################################################################
+# CS:APP Malloc Lab
+# 학생용 안내 문서 (한국어)
+#
+# Copyright (c) 2002, R. Bryant and D. O'Hallaron, All rights reserved.
+# 허가 없이 사용, 수정 또는 복사할 수 없습니다.
+#
+######################################################################
+
+***********
+주요 파일:
+***********
+
+mm.{c,h}
+	여러분이 구현할 malloc 패키지입니다. 제출할 파일은 mm.c이며,
+	수정해야 하는 유일한 파일도 mm.c입니다.
+
+mdriver.c
+	mm.c 파일을 테스트하는 malloc 드라이버입니다.
+
+short{1,2}-bal.rep
+	시작을 돕기 위한 작은 trace 파일 두 개입니다.
+
+Makefile
+	드라이버를 빌드합니다.
+
+**********************************
+드라이버를 위한 기타 지원 파일
+**********************************
+
+config.h	Configures the malloc lab driver / malloc lab 드라이버를 설정합니다.
+fsecs.{c,h}	서로 다른 타이머 패키지를 감싸는 래퍼 함수입니다.
+clock.{c,h}	Pentium 및 Alpha 사이클 카운터에 접근하는 루틴입니다.
+fcyc.{c,h}	사이클 카운터 기반 타이머 함수입니다.
+ftimer.{c,h}	interval timer와 gettimeofday() 기반 타이머 함수입니다.
+memlib.{c,h}	힙과 sbrk 함수를 모델링합니다.
+
+*******************************
+드라이버 빌드 및 실행
+*******************************
+
+드라이버를 빌드하려면 셸에서 `make`를 입력하세요.
+
+작은 테스트 trace로 드라이버를 실행하려면:
+
+	unix> mdriver -V -f short1-bal.rep
+
+`-V` 옵션은 유용한 추적 정보와 요약 정보를 출력합니다.
+
+드라이버 플래그 목록을 보려면:
+
+	unix> mdriver -h
+
--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -1,13 +1,13 @@
 /*
- * mm-naive.c - The fastest, least memory-efficient malloc package.
+ * mm-naive.c - The fastest, least memory-efficient malloc package. / mm-naive.c - 가장 빠르지만 메모리 효율은 가장 낮은 malloc 패키지.
  *
- * In this naive approach, a block is allocated by simply incrementing
- * the brk pointer.  A block is pure payload. There are no headers or
- * footers.  Blocks are never coalesced or reused. Realloc is
- * implemented directly using mm_malloc and mm_free.
+ * In this naive approach, a block is allocated by simply incrementing / 이 단순한 방식에서는 블록을 단순히 증가시키는 방식으로 할당한다.
+ * the brk pointer.  A block is pure payload. There are no headers or / brk 포인터를 증가시킨다. 블록은 순수 payload만 가지며, 헤더나
+ * footers.  Blocks are never coalesced or reused. Realloc is / 푸터가 없다. 블록은 병합되거나 재사용되지 않으며, realloc은
+ * implemented directly using mm_malloc and mm_free. / mm_malloc과 mm_free를 직접 사용해 구현된다.
  *
- * NOTE TO STUDENTS: Replace this header comment with your own header
- * comment that gives a high level description of your solution.
+ * NOTE TO STUDENTS: Replace this header comment with your own header / 학생들에게: 이 헤더 주석을 여러분의 방식에 대한
+ * comment that gives a high level description of your solution. / 상위 수준 설명을 담은 주석으로 바꾸세요.
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,40 +19,57 @@
 #include "memlib.h"
 
 /*********************************************************
- * NOTE TO STUDENTS: Before you do anything else, please
- * provide your team information in the following struct.
+ * NOTE TO STUDENTS: Before you do anything else, please / 학생들에게: 다른 작업을 하기 전에 먼저
+ * provide your team information in the following struct. / 아래 구조체에 팀 정보를 입력하세요.
  ********************************************************/
 team_t team = {
-    /* Team name */
+    /* Team name / 팀 이름 */
     "ateam",
-    /* First member's full name */
+    /* First member's full name / 첫 번째 팀원의 전체 이름 */
     "Harry Bovik",
-    /* First member's email address */
+    /* First member's email address / 첫 번째 팀원의 이메일 주소 */
     "bovik@cs.cmu.edu",
-    /* Second member's full name (leave blank if none) */
+    /* Second member's full name (leave blank if none) / 두 번째 팀원의 전체 이름 (없으면 비워 두세요) */
     "",
-    /* Second member's email address (leave blank if none) */
+    /* Second member's email address (leave blank if none) / 두 번째 팀원의 이메일 주소 (없으면 비워 두세요) */
     ""};
 
-/* single word (4) or double word (8) alignment */
+/* single word (4) or double word (8) alignment / single word(4) 또는 double word(8) 단위 정렬 */
 #define ALIGNMENT 8
 
-/* rounds up to the nearest multiple of ALIGNMENT */
+/* rounds up to the nearest multiple of ALIGNMENT / ALIGNMENT의 가장 가까운 배수로 올림 */
 #define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+#define WSIZE 4
+#define DSIZE 8
+#define CHUNKSIZE (1 << 12)
+#define PACK(size, alloc) ((size) | (alloc))
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p, val) (*(unsigned int *)(p) = (val))
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE((char *)(bp) - DSIZE))
+
+static char *heap_listp;
+
 /*
- * mm_init - initialize the malloc package.
+ * mm_init - initialize the malloc package. / mm_init - malloc 패키지를 초기화한다.
  */
 int mm_init(void)
 {
+    
+    
     return 0;
 }
 
 /*
- * mm_malloc - Allocate a block by incrementing the brk pointer.
- *     Always allocate a block whose size is a multiple of the alignment.
+ * mm_malloc - Allocate a block by incrementing the brk pointer. / mm_malloc - brk 포인터를 증가시켜 블록을 할당한다.
+ *     Always allocate a block whose size is a multiple of the alignment. / 항상 정렬 단위의 배수 크기를 갖는 블록을 할당한다.
  */
 void *mm_malloc(size_t size)
 {
@@ -68,14 +85,14 @@ void *mm_malloc(size_t size)
 }
 
 /*
- * mm_free - Freeing a block does nothing.
+ * mm_free - Freeing a block does nothing. / mm_free - 블록을 해제해도 아무 일도 하지 않는다.
  */
 void mm_free(void *ptr)
 {
 }
 
 /*
- * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
+ * mm_realloc - Implemented simply in terms of mm_malloc and mm_free / mm_realloc - mm_malloc과 mm_free를 이용해 단순하게 구현된다.
  */
 void *mm_realloc(void *ptr, size_t size)
 {
